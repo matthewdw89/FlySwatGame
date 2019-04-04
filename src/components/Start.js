8096ae14fe3069cf7576d056fb382ea8679031c6
Matt Waters <matthewdw89@gmail.com>
2019-04-03 20:04:41 -0400

    some test cases added for the insect component

--- src/components/Start.js
+++ src/components/Start.js
@@ -4,7 +4,6 @@ export default class Start extends Component {
   render() {
     return (
       <div className="start__window">
-        {/* <StartingMusic></StartingMusic> */}
         <div className="start__box">
             <h1>SWAT</h1>
             <button title="The aim of the game is remove all the pesky bugs within a set timer" onClick={this.props.gameStart}>START</button>
--- src/test/insect.test.js
+++ src/test/insect.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Insect from '../components/Insect';
+
+configure({ adapter: new Adapter() });
 
 describe('<Insect>', () => {
 
@@ -8,4 +11,12 @@ describe('<Insect>', () => {
     const component = shallow(<Insect />);
     expect(component).toMatchSnapshot();
   });
+
+  it('should register a click', () => {
+    let clickFn = jest.fn();
+    const component = shallow(<Insect hit={clickFn}/>);
+    component.simulate('click')
+    expect(clickFn).toHaveBeenCalled();
+  });
+
 });
